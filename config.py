4e483a4192fbf739a57e587f27b73a8c40251d27
dev <main.develop@protonmail.com>
2024-11-19 20:49:31 +0100

    Initial database create

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(50), nullable=True)
+    last_name = db.Column(db.String(50), nullable=True)
+    gender = db.Column(db.String(14), nullable=True)
+
+
+    def __repr__(self):
+        return f"<User {self.email}>"
