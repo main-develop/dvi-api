b3fe0ced92fc2b2e5d9b379b8ae72c198b35fd9c
dev <main.develop@protonmail.com>
2024-12-12 13:17:34 +0100

    Added blacklist for revoking JWTs

--- app.py
+++ app.py
@@ -2,22 +2,30 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+import redis
 
 from config import Config
 from models import db
-from routes.authentication import authentication
+from routes.authentication import authentication, init_jwt_token_loader
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    app.register_blueprint(authentication, url_prefix="/dvi-api/authentication")
 
     db.init_app(app)
-
     migrate = Migrate(app, db)
     bcrypt = Bcrypt(app)
+    CORS(app)
+
     jwt = JWTManager(app)
+
+    redis_client = redis.Redis(host="redis", port=6379, decode_responses=True)
+    app.redis_client = redis_client
+
+    app.register_blueprint(authentication, url_prefix="/dvi-api/authentication")
+    init_jwt_token_loader(jwt)
 
     return app
 
--- routes/authentication.py
+++ routes/authentication.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, request, jsonify
-from flask_jwt_extended import create_access_token
+from flask import Blueprint, request, jsonify, current_app
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 from models import db, User
+from datetime import timedelta
 
 authentication = Blueprint("authentication", __name__)
 
@@ -41,14 +42,35 @@ def register():
 def login():
     data = request.json
 
-    email = data.get("email")
-    password = data.get("password")
+    email = data["email"]
+    password = data["password"]
+    remember_me = data["rememberMe"]
 
     user = User.query.filter_by(email=email).first()
 
     if not user or not user.check_password(password):
         return jsonify({"error": "Invalid email or password."}), 401
-    
-    access_token = create_access_token(identity=user.id)
 
-    return jsonify({"message": "Login successful.", "access_token": access_token}), 200
+    access_token = create_access_token(identity=user.id, expires_delta=timedelta(days=5) if remember_me else None)
+
+    return jsonify({"message": "Login successful.", "accessToken": access_token}), 200
+
+
+@authentication.route("/logout", methods=["POST"])
+@jwt_required()
+def logout():
+    jti = get_jwt()["jti"]
+
+    redis_client = current_app.redis_client
+    redis_client.set(jti, "", ex=timedelta(days=2))
+
+    return jsonify({"message": "Successfully logged out."}), 200
+
+
+def init_jwt_token_loader(jwt):
+    @jwt.token_in_blocklist_loader
+    def check_if_token_is_revoked(jwt_header, jwt_payload):
+        redis_client = current_app.redis_client
+        jti = jwt_payload["jti"]
+
+        return redis_client.exists(jti) > 0
